fabcedda695868659532886578ab691b2aafd043
sopyansyauri <rifaii.sopyansyauri04@gmail.com>
2024-03-07 19:26:00 +0700

    add file_read function

--- main.c
+++ main.c
@@ -1,44 +1,72 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 
 #define D 3
 #define U 20
 #define H 7
 
-void encrypt(char data[], char data2[]) {
+void encrypt(char data[], char data2[])
+{
 
-    for (int i = 0; i < strlen(data); i += 3) {
+    for (int i = 0; i < strlen(data); i += 3)
+    {
         data2[i] = data[i] + D;
     }
 
-    for (int j = 1; j < strlen(data); j += 3) {
+    for (int j = 1; j < strlen(data); j += 3)
+    {
         data2[j] = data[j] + U;
     }
 
-    for (int k = 2; k < strlen(data); k += 3) {
+    for (int k = 2; k < strlen(data); k += 3)
+    {
         data2[k] = data[k] + H;
     }
-
 }
 
-void decrypt(char data[], char data2[]) {
+void decrypt(char data[], char data2[])
+{
 
-    for (int i = 0; i < strlen(data); i += 3) {
+    for (int i = 0; i < strlen(data); i += 3)
+    {
         data2[i] = data[i] - D;
     }
 
-    for (int j = 1; j < strlen(data); j += 3) {
+    for (int j = 1; j < strlen(data); j += 3)
+    {
         data2[j] = data[j] - U;
     }
 
-    for (int k = 2; k < strlen(data); k += 3) {
+    for (int k = 2; k < strlen(data); k += 3)
+    {
         data2[k] = data[k] - H;
     }
-
 }
 
-int main(void) {
+void file_read(char *buffer, char *file_name)
+{
+    buffer = NULL;
+
+    FILE *file = fopen(file_name, "r");
+
+    fseek(file, 0, SEEK_END);
+
+    size_t file_size = ftell(file);
+
+    buffer = malloc(file_size);
+
+    fseek(file, 0, SEEK_SET);
+
+    fread(buffer, 1, file_size, file);
+
+    fclose(file);
+
+    printf("%s \n", buffer);
+}
+
+int main(void)
+{
     char name[] = "data berharga";
     char encryptText[sizeof(name)];
     char decryptText[sizeof(name)];
@@ -47,13 +75,9 @@ int main(void) {
 
     decrypt(encryptText, decryptText);
 
-
     printf("plain text \t\t: %s \n", name);
     printf("encrypt text \t\t: %s \n", encryptText);
     printf("decrypt text \t\t: %s \n", decryptText);
-
-
-
 
     return 0;
 }
