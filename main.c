15fe5382bbcbc9c0a82db8eee09a6c0203b2dae3
sopyansyauri <rifaii.sopyansyauri04@gmail.com>
2024-08-26 07:55:20 +0700

    menambahkan '\0' di array

--- main.c
+++ main.c
@@ -6,70 +6,53 @@
 #define U 20
 #define H 7
 
-void encrypt(char data[], char data2[])
+void encrypt(char *variable_src, char *variable_dest)
 {
 
-    for (int i = 0; i < strlen(data); i += 3)
+    for (int i = 0; i < strlen(variable_src); i += 3)
     {
-        data2[i] = data[i] + D;
+        variable_dest[i] = variable_src[i] + D;
     }
 
-    for (int j = 1; j < strlen(data); j += 3)
+    for (int j = 1; j < strlen(variable_src); j += 3)
     {
-        data2[j] = data[j] + U;
+        variable_dest[j] = variable_src[j] + U;
     }
 
-    for (int k = 2; k < strlen(data); k += 3)
+    for (int k = 2; k < strlen(variable_src); k += 3)
     {
-        data2[k] = data[k] + H;
+        variable_dest[k] = variable_src[k] + H;
     }
 }
 
-void decrypt(char data[], char data2[])
+void decrypt(char *variable_src, char *variable_dest)
 {
 
-    for (int i = 0; i < strlen(data); i += 3)
+    for (int i = 0; i < strlen(variable_src); i += 3)
     {
-        data2[i] = data[i] - D;
+        variable_dest[i] = variable_src[i] - D;
     }
 
-    for (int j = 1; j < strlen(data); j += 3)
+    for (int j = 1; j < strlen(variable_src); j += 3)
     {
-        data2[j] = data[j] - U;
+        variable_dest[j] = variable_src[j] - U;
     }
 
-    for (int k = 2; k < strlen(data); k += 3)
+    for (int k = 2; k < strlen(variable_src); k += 3)
     {
-        data2[k] = data[k] - H;
+        variable_dest[k] = variable_src[k] - H;
     }
 }
 
-void file_read(char *buffer, char *file_name)
+int main(int argc, char *argv[])
 {
-    buffer = NULL;
-
-    FILE *file = fopen(file_name, "r");
-
-    fseek(file, 0, SEEK_END);
-
-    size_t file_size = ftell(file);
-
-    buffer = malloc(file_size);
-
-    fseek(file, 0, SEEK_SET);
-
-    fread(buffer, 1, file_size, file);
-
-    fclose(file);
-
-    printf("%s \n", buffer);
-}
-
-int main(void)
-{
-    char name[] = "data berharga";
-    char encryptText[sizeof(name)];
-    char decryptText[sizeof(name)];
+    char *name = "DATA INI SANGAT BERHARGA DAN TIDAK BISA DI BACA";
+    int panjang = strlen(name);
+    char encryptText[panjang];
+    char decryptText[panjang];
+    encryptText[panjang] = '\0';
+    decryptText[panjang] = '\0';
+    int i;
 
     encrypt(name, encryptText);
 
